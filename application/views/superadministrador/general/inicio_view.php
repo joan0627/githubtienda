0be5642927e547fd91dfa265043bdc9ffa5bba32
Luis David Sánchez <61328996+Kingcano124@users.noreply.github.com>
2020-12-13 23:06:14 -0500

    final lista
    Lista

--- application/views/superadministrador/general/inicio_view.php
+++ application/views/superadministrador/general/inicio_view.php
@@ -88,7 +88,7 @@
                         <div class="icon">
                             <i class="ion ion-cash"></i>
                         </div>
-                        <a href="#" class="small-box-footer">Más <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="<?php echo base_url();?>venta/ventaproductos" class="small-box-footer">Más <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
